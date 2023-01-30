69a293e2c8908b62d30822736eec3ecd3adac73f
Martina Yusuf <martina.yusuf2@gmail.com>
2023-01-30 11:45:17 +0000

    Intro to Python

---
+++ intro.py
@@ -0,0 +1,37 @@
+print("Hello World!")
+# create variable x and assign it a string value
+x = "Martina"
+
+# reassigning the value of x to an integer value
+x = 5
+
+y = 7
+
+print(x+y)
+
+# camel case
+firstName = "Martina"
+lastName = "Yusuf"
+
+# concatenation
+print(str(x) + " " + lastName)
+
+# print string {{number}} of times
+print(lastName * x)
+
+# turn the argument into a string
+# changing type
+# str()
+
+print(type(x))
+
+myBool = True
+print(type(myBool))
+
+myNum = 5
+# these two lines are equivalent
+myNum = myNum + 1
+myNum += 1
+
+
+
