43326b1a2e4f371dfe066d1a4cdd7e0fa45e23d5
Martina Yusuf <martina.yusuf2@gmail.com>
2023-06-23 15:55:26 +0100

    friday intro

---
+++ friday.py
@@ -0,0 +1,62 @@
+print("hello world!")
+print("It's Friday! Yay!")
+
+myName = "Santa Claus"
+
+print(myName)
+print("What's your name? My name is:", myName)
+
+# myNum = 7.6
+#
+# age = 17
+#
+# print("I'm ", age,   "years old")
+#
+# isRaining = True
+#
+# print( type(isRaining) )
+# print(type(myNum))
+# print(type(myName))
+
+# print( myName.upper() )
+
+
+# num1 = 5
+# num2 = 8
+# print(num1 + num2)
+# print(num1 - num2)
+# # modulo
+# # useful to find out if numbers are odd or even - modulo 2
+# print(num2 % 2)
+#
+# print(num2>num1)
+#
+# firstName = "Martina"
+# lastName = "Yusuf"
+# print(firstName +" "+ lastName)
+#
+# # print(num1 + firstName)
+#
+# print(num1*firstName)
+#
+# result = num1+num2
+#
+# num = num1 + 1
+# # shortcut
+# num1 += 1
+#
+# print(num+1)
+#
+# myList=["Martina", "Yusuf", "socks"]
+# print(myList[0])
+# myList[1]="t-shirt"
+#
+# myTuple=("Martina", "yusuf", "Socks")
+# print(myTuple[1])
+# myTuple[1]="t-shirt"
+
+my_choice = input("Please type in a number : ")
+print("You have typed ", my_choice)
+print(type(my_choice))
+my_choice_int = int(my_choice)
+print(type(my_choice_int))
